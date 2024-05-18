a8a173bf7ecd366e08c9b0559ba0607e6e77b792
tamiopia <tamiopia23@gmail.com>
2024-05-18 16:25:16 +0300

    this is test

--- index.js
+++ index.js
@@ -41,7 +41,11 @@ app.post('/checkout', async (req, res) => {
         });
 
         console.log(session);
-        res.json({ id: session.id });
+        res.json({ id: session.id,
+            data:session
+
+
+        });
     } catch (error) {
         console.error('Error creating checkout session:', error);
         res.status(500).send(error.message);
